a114d168667133d920b623d98d66ee78b5281597
ritawi8 <rita.wildrot@gmail.com>
2025-12-11 15:18:54 +0100

    Update homepage layout and content for Wellness Studio introduction

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,14 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
-    <div className="flex w-full max-w-3xl flex-col items-center gap-6 px-8 py-24 text-center sm:items-start sm:text-left">
-      <h1 className="max-w-xs text-3xl leading-10 tracking-tight">
-        To get started, edit the page.tsx file.
+    <main className="flex min-h-screen justify-center pt-40 px-6">
+    <div className="flex w-full max-w-3xl flex-col items-center gap-6 px-8  text-center sm:items-start sm:text-left ">
+      <h1 className="text-5xl leading-tight font-semibold">
+        Welcome to Wellness Studio
       </h1>
-      <p>
-        Här kan du börja bygga din FlowPass / Wellness-sida.
+      <p className="text-lg">
+        FlowPass is your NFT membership pass for modern wellness spaces 
+        <br />- simple, secure and made for real people, not crypto nerd.
       </p>
     </div>
   </main>
